b64e169218b53293eb805602e3f6c7efd5cfd2ea
paolchol <paolocolombo1996@gmail.com>
2021-06-08 17:32:03 +0200

    added code to manage extraction data, created daily extraction dataset

--- Data_import_cloud.py
+++ Data_import_cloud.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+☺# -*- coding: utf-8 -*-
 """
 Data import from KWR's cloud
 
@@ -112,6 +112,8 @@ NA_count_up = pd.read_table(path)
 NA_count_up.index = NA_count_up.iloc[:,0]; NA_count_up.index.names = ['Station']
 NA_count_up.drop('Unnamed: 0', axis=1, inplace=True)
 
+years_data = round(NA_count_up.iloc[:,0]/100*NA_count_up.iloc[:,1]/365)
+      
 ## LOGGERS metadata
 path = r'\\nwg.local\dfs\projectdata\P402045_238\ReceivedData\DataNZV\LOGGERS\LOGGERS\Metadata_loggers_OW_GW.xlsx'
 log_meta = pd.read_excel(path)
@@ -139,13 +141,14 @@ log_dates.loc[:,'EIND'].fillna(pd.to_datetime('11-11-2020'), inplace = True)
 log_dates.loc[:,'START'] = pd.to_datetime(log_dates.loc[:,'START'])
 
 round(max(log_dates.loc[:,'EIND']-log_dates.loc[:,'START']).days/365) #42 years (not cleaned)
-s = round(len(log_dates)*10/100) #number of stations to extract
+s = round(len(log_dates)*10/100) #number of stations to extract to calculate the threshold
 delta = log_dates.loc[:,'EIND']-log_dates.loc[:,'START']
 #"Clean" the deltas by considering the missing data percentage
 delta_clean = delta.dt.days*(NA_count_up.iloc[:,0].values/100)
 mean_delta = sum(sorted(delta_clean)[-s:])/s
 # threshold = round(mean_delta) #7882 days
 threshold = 20*365 #20 years threshold
+threshold = 13*365 #14 years threshold
 print(f'Threshold: {threshold} days, {round(threshold/365)} \
 years\nNumber of stations which satisfy the threshold: \
 {len(delta_clean[delta_clean >= threshold])}')
@@ -159,7 +162,8 @@ years\nNumber of stations which satisfy the threshold: \
 ## Dataframe construction
 ### Date range setting
 #1980-2020 to start, then we can change the range
-start = datetime.datetime(1980, 1, 1)
+end_year = 2020
+start = datetime.datetime(end_year-int(threshold/365), 1, 1)
 end = datetime.datetime(2020, 11, 11) #the loggers data end at 11/11/2020
 date = pd.date_range(start = start, end = end)
 date = date.strftime('%Y-%m-%d')
@@ -174,6 +178,12 @@ print(end - start)
 #In create_loggers_dataframe, the threshold on the minimum series length is used
 #to select only station who have at least that number of measurements.
 #It took 38 seconds to create a dataframe with a 22 years threshold
+#It took 79 seconds to create a dataframe with a 20 years threshold
+
+#### Save the dataframe as a csv
+#And also try to save it as a pickle
+path = r'D:\Users\colompa\Documents\KWR_Internship\Data\logger_GW_13y.csv'
+loggers_GW.to_csv(path_or_buf = path, sep=",", index = False)
 
 ## Extract the coordinates and visualize the stations
 log_coord = log_meta.loc[:,['LOCATION', 'X', 'Y']]
@@ -199,18 +209,61 @@ log_coord_thre = log_coord[delta_clean >= threshold]
 #Export as .txt files
 path = r'D:\Users\colompa\Documents\KWR_Internship\Data\log_coord.txt'
 log_coord.to_csv(path_or_buf = path, sep = "\t", index = False)
-path = r'D:\Users\colompa\Documents\KWR_Internship\Data\log_coord_thre.txt'
+path = r'D:\Users\colompa\Documents\KWR_Internship\Data\log_coord_thre_13y.txt'
 log_coord_thre.to_csv(path_or_buf = path, sep = "\t", index = False)
 
 
-
 # Extraction data
-# Arrange the paths to the files in the cloud
-## File from D2extraction
-path = r'\\nwg.local\dfs\projectdata\P402045_238\ReceivedData\DataNZV\D2extraction\Singlepoint'
-extr_file = []
-extr_file = create_list_files(extr_file, path, '.csv')
+## Arrange the information in the file
+path = r'\\nwg.local\dfs\projectdata\P402045_238\ReceivedData\DataNZV\D2extraction\Singlepoint\ExtractionsSinglePointWithCoords.csv'
+extraction_file = pd.read_csv(path, sep = ";")
 
+#Extract values and positions
+columns = ['Sublocation_ID', 'EXTR_m3y_2010', 'EXTR_m3y_2011', 'EXTR_m3y_2012', 'EXTR_m3y_2013', 'EXTR_m3y_2014', 'EXTR_m3y_2015', 'EXTR_m3y_2016', 'EXTR_m3y_2017', 'EXTR_m3y_2018', 'EXTR_m3y_2019', 'EXTR_m3y_2020']
+extraction_values = extraction_file.loc[:,columns]
+columns = ['Sublocation_ID', 'X', 'Y']
+extraction_positions = extraction_file.loc[:,columns]
 
-#Please update
+#Put date in rows and station in columns
+extraction_values = extraction_values.transpose()
+extraction_values.columns = extraction_values.iloc[0,:]
+extraction_values.drop(index = 'Sublocation_ID', inplace = True)
+start = datetime.datetime(2010, 1, 1)
+end = datetime.datetime(2021, 1, 1) #the loggers data end at 11/11/2020
+extraction_values.index =  pd.date_range(start = start, end = end, freq = 'Y')
+
+## Obtain daily data by dividing the yearly data by the length of that year
+
+def daily_extraction(station, name = 'data'):
+    import calendar as cl
+    station = pd.to_numeric(station, errors='coerce')
+    for i in range(len(station)):
+        num_days = 365
+        if cl.isleap(station.index[i].year):
+            num_days = 366
+        start = datetime.datetime(station.index[i].year, 1, 1)
+        date = pd.date_range(start = start, end = station.index[i])
+        new_year = pd.DataFrame({"date": date})
+        if station.iloc[i] is None:
+            new_year[name] = nan
+        else:
+            daily = station.iloc[i]/num_days
+            new_year[name] = daily
+        if i == 0: df = new_year
+        else: df = df.append(new_year)
+    return df
+
+for i, station in enumerate(extraction_values, start = 0):
+    if i > 0:
+        dd = daily_extraction(extraction_values.iloc[:,i], str(extraction_values.columns[i]))
+        extr_daily[station] = dd.iloc[:,1]
+    else:
+        extr_daily = daily_extraction(extraction_values.iloc[:,i], str(extraction_values.columns[i]))
+
+## Save the dataframe
+path = r'D:\Users\colompa\Documents\KWR_Internship\Data\extr_daily.csv'
+extr_daily.to_csv(path_or_buf = path, sep = "\t", index = False)
+
+## Select the extraction points near the target points
+
 
