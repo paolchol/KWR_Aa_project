7c150a407be4717eb053e0084bd06db573e079ef
paolchol <paolocolombo1996@gmail.com>
2021-07-27 16:40:30 +0200

    updates to align to the updated functions

--- Main_folder/main_model.py
+++ Main_folder/main_model.py
@@ -42,33 +42,41 @@ near_e = pkl.load(open('near_evap.p', 'rb'))
 #Remove outliers precipitation data
 df_prec = dp.remove_outliers(df_prec, 0)
 
+#Load the model parameters
+m_par = pkl.load(open('model_parameters.p', 'rb'))
+
 # %% Model launch
 
 i = 0
+predictions = {}
+observations = {}
 for name, _ in SSA_components.items():
+    #Trend
     trend = dp.extract_attributes(SSA_components[name], SSA_information[i], 't')
     df_trend = dp.create_dataframe(trend, df_prec, df_evap, near_p, near_e)
-    
-    #Dataset operations
-    
-    #Data windowing
-    
-    pred_t = launch_model(df_trend)
-    #launch_model will have to return a series
-    
-    
-    #Same for yper, mper and noise
+    t_yhat, t_y, t_fitted = md.launch(df_trend, m_par[i], 't')
+    #Yearly periodicity
     yper = dp.extract_attributes(SSA_components[name], SSA_information[i], 'yp')
+    df_yper = dp.create_dataframe(yper, df_prec, df_evap, near_p, near_e)
+    y_yhat, y_y, y_fitted = md.launch(df_yper, m_par[i], 'yp')
+    #Monthly periodicity
     mper = dp.extract_attributes(SSA_components[name], SSA_information[i], 'mp')
+    df_mper = dp.create_dataframe(mper, df_prec, df_evap, near_p, near_e)
+    m_yhat, m_y, m_fitted = md.launch(df_mper, m_par[i], 'mp')
+    #Noise
     noise = dp.extract_attributes(SSA_components[name], SSA_information[i], 'n')
+    df_noise = dp.create_dataframe(noise, df_prec, df_evap, near_p, near_e)
+    n_yhat, n_y, n_fitted = md.launch(df_noise, m_par[i], 'n')
     
-    prediction = pred_t + pred_yp + pred_mp + pred_noise
+    #Complete prediction
+    predictions[i] = t_yhat + y_yhat + m_yhat + n_yhat
+    observations[i] = t_y + y_y + m_y + n_y
+    
+    i = i + 1
+    
     #Prediction doesn't have the date information anymore like this
     #It can be taken from SSA_components[name].orig_TS.index, and add the two weeks ahead
     #Prediction can be stored in a dictionary, to keep all the 20 runs for later
-    
-    
-    i = i + 1
 
 
 # %% Visualize
